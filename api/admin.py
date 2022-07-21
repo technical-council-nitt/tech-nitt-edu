917d6fa3a48a430d9d74c998b988e928b1f6ed75
Guruprasanna02 <guruprasannavasudevan02@gmail.com>
2022-07-22 03:02:43 +0530

    add: club CMS

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,20 @@
+from datetime import datetime
 from django.contrib import admin
 
+# Register your models here.Clubs
+from .models import Club
+
+# admin.site.register(Question)
+# admin.site.register(Choice)
+
+admin.site.site_header = "Tech NITT Admin"
+admin.site.site_title = "Tech NITT Admin Area"
+admin.site.index_title = "Welcome to the Tech NITT Admin Area"
 # Register your models here.
+
+
+class ClubAdmin(admin.ModelAdmin):
+    fieldsets = [(None, {'fields': ['clubName']}), (None, {'fields': ['clubDescription']}), (None, {'fields': ['adminRollNo']}), (None, {'fields': ['clubImage']}), (None, {'fields': ['clubLogo']}), (None, {'fields': ['clubLinks']}), (None, {'fields': ['clubProjects']}), (None, {'fields': ['clubMembers']})]
+
+
+admin.site.register(Club, ClubAdmin)
--- api/models.py
+++ api/models.py
@@ -1,44 +1,40 @@
-from unittest.util import _MAX_LENGTH
 from django.db import models
 
 
 # SCHEMA FOR CLUBS
-class Clubs(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.TextField(max_length=1e4)
+class Club(models.Model):
+    clubName = models.CharField(max_length=100)
+    clubDescription = models.TextField(max_length=1e4)
 
     # 9 digit roll no
     adminRollNo = models.CharField(max_length=9)
 
-    image = models.ImageField(upload_to='images/', default='images/default.png')
-    logo = models.ImageField(upload_to='logos/', default='logos/default.png')
+    clubImage = models.ImageField(upload_to='images/', default='images/default.png')
+    clubLogo = models.ImageField(upload_to='logos/', default='logos/default.png')
 
-    projects = [models.ForeignKey('Projects', on_delete=models.PROTECT)]
-    members = [models.ForeignKey('Members', on_delete=models.PROTECT)]
-    links = [models.CharField(max_length=100)]
+    clubProjects = models.ManyToManyField('Project')
+    clubMembers = models.ManyToManyField('Member')
+    clubLinks = models.CharField(max_length=100)
 
     # last updated timestamp
-    updated_at = models.DateTimeField(auto_now=True)
-
-
-# SCHEMA FOR PROJECTS (UNDER CLUBS)
-class Projects(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.TextField(max_length=1e4)
-    image = models.ImageField(upload_to='images/', default='images/default.png')
-    links = [models.CharField(max_length=100)]
-    club = models.ForeignKey('Clubs', on_delete=models.PROTECT)
-    members = [models.ForeignKey('Members', on_delete=models.PROTECT)]
-    links = [models.CharField(max_length=100)]
-    # last updated timestamp
-    updated_at = models.DateTimeField(auto_now=True)
-
+    club_updated_at = models.DateTimeField(auto_now=True)
 
 # SCHEMA FOR MEMBERS - (USED UNDER PROJECTS AND CLUBS)
-class Members(models.Model):
-    name = models.CharField(max_length=100)
-    rollNo = models.CharField(max_length=9)
-    image = models.ImageField(upload_to='images/', default='images/default.png')
-    # club = models.ForeignKey('Clubs', on_delete=models.PROTECT)
-    # projects = [models.ForeignKey('Projects', on_delete=models.PROTECT)]
-    # links = [models.CharField(max_length=100)]
+class Member(models.Model):
+    memberName = models.CharField(max_length=100)
+    memberRollNo = models.CharField(max_length=9)
+    memberImage = models.ImageField(upload_to='images/', default='images/default.png')
+    memberClub = models.ForeignKey('Club', on_delete=models.PROTECT)
+    memberProjects = models.ManyToManyField('Project')
+    memberLinks = models.CharField(max_length=100)
+
+# SCHEMA FOR PROJECTS (UNDER CLUBS)
+class Project(models.Model):
+    projectName = models.CharField(max_length=100)
+    projectDescription = models.TextField(max_length=1e4)
+    projectImage = models.ImageField(upload_to='images/', default='images/default.png')
+    projectLinks = models.CharField(max_length=100)
+    projectClub = models.ForeignKey('Club', on_delete=models.PROTECT)
+    projectMembers = models.ManyToManyField('Member')
+    # last updated timestamp
+    project_updated_at = models.DateTimeField(auto_now=True)
